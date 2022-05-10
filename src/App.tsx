de6dba92d8cc9a1036c0f98af40ae32f71d66c1e
Oleg Somov <gog0125@mail.ru>
2022-05-10 23:03:00 +0300

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Header } from './components';
+
+// TODO: вместо ссылки на профиль показывать предложение войти в аккаунт когда пользователь не залогинен
 
 function App() {
-  return <div></div>;
+  return (
+    <div className='app'>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
 import { store } from './app/store';
 import App from './App';
 
 import 'normalize.css';
-import './index.scss';
+import './styles/app.scss';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
