dc661c10daa4fdfcadc913a18f5933aa95b25221
Oleg Somov <gog0125@mail.ru>
2022-05-11 10:21:50 +0300

    feat: add main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 import { Header } from './components';
+import { Main } from './pages';
 
 // TODO: вместо ссылки на профиль показывать предложение войти в аккаунт когда пользователь не залогинен
 
@@ -7,6 +10,9 @@ function App() {
   return (
     <div className='app'>
       <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,161 @@
+import React from 'react';
+
+import classes from './Main.module.scss';
+
+export const Main = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.category}>
+        <h2 className={classes.category__title}>Новые Фильмы</h2>
+        <div className={classes.cards}>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.category}>
+        <h2 className={classes.category__title}>Новые Сериалы</h2>
+        <div className={classes.cards}>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.category}>
+        <h2 className={classes.category__title}>Новые Игры</h2>
+        <div className={classes.cards}>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.category}>
+        <h2 className={classes.category__title}>Новые Книги</h2>
+        <div className={classes.cards}>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+          <div className={classes.card}>
+            <img
+              className={classes.card__img}
+              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
+              alt='movie name'
+            />
+            <h3 className={classes.card__name}>Movie Name</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
