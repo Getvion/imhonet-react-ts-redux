d2d1c22a1015e1b90067045e6a7b1fa95a45c55b
Oleg Somov <gog0125@mail.ru>
2022-05-12 19:12:54 +0300

    feat: add slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 
 export const Slider = () => {
   return (
-    <div>Slider</div>
-  )
-}
+    <Carousel centerMode={true} infiniteLoop={true}>
+      <div>
+        <img
+          src='https://media.rawg.io/media/games/4be/4be6a6ad0364751a96229c56bf69be59.jpg'
+          alt='slide'
+        />
+        <p className='legend'>Legend 1</p>
+      </div>
+      <div>
+        <img
+          src='https://media.rawg.io/media/games/4a0/4a0a1316102366260e6f38fd2a9cfdce.jpg'
+          alt='slide'
+        />
+        <p className='legend'>Legend 2</p>
+      </div>
+      <div>
+        <img
+          src='https://media.rawg.io/media/games/b7d/b7d3f1715fa8381a4e780173a197a615.jpg'
+          alt='slide'
+        />
+        <p className='legend'>Legend 3</p>
+      </div>
+    </Carousel>
+  );
+};
--- src/components/UI/Dropdown/Dropdown.tsx
+++ src/components/UI/Dropdown/Dropdown.tsx
@@ -14,19 +14,24 @@ export const Dropdown: React.FC<IDropdown> = ({ username }) => {
     setIsDropdownVisible(!isDropdownVisible);
   };
   return (
-    <li className={classes.nav__item} onClick={toggleDropdown}>
-      {username}
-      {isDropdownVisible && (
-        <div className={classes.dropdown}>
-          <Link className={classes.dropdown__item} to={'/profile'}>
-            {username}
-          </Link>
-          <Link className={classes.dropdown__item} to={'/settings'}>
-            Настройки
-          </Link>
-          <button className={`${classes.dropdown__item} ${classes.dropdown__logout}`}>Выйти</button>
-        </div>
-      )}
-    </li>
+    <>
+      {isDropdownVisible && <div onClick={toggleDropdown} className={classes.wrap}></div>}
+      <li className={classes.nav__item} onClick={toggleDropdown}>
+        {username}
+        {isDropdownVisible && (
+          <div className={classes.dropdown}>
+            <Link className={classes.dropdown__item} to={'/profile'}>
+              {username}
+            </Link>
+            <Link className={classes.dropdown__item} to={'/settings'}>
+              Настройки
+            </Link>
+            <button className={`${classes.dropdown__item} ${classes.dropdown__logout}`}>
+              Выйти
+            </button>
+          </div>
+        )}
+      </li>
+    </>
   );
 };
