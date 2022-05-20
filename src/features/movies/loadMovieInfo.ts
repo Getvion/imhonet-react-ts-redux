6ad439c550ac6748e3b453f65ea9fa85ba9d1a2a
Oleg Somov <gog0125@mail.ru>
2022-05-20 15:23:20 +0300

    add new request for movie data

---
+++ src/features/movies/loadMovieInfo.ts
@@ -0,0 +1,45 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const loadMovieInfo = createAsyncThunk(
+  'load-movie-info',
+  async (filmId: number | string, { extra: { client, requests } }: any) => {
+    return await client.get(requests.GET_MOVIE_INFO_BY_ID(filmId), {
+      headers: {
+        'X-API-KEY': requests.MOVIES_API_KEY,
+      },
+    });
+  }
+);
+
+export const loadMovieStaffInfo = createAsyncThunk(
+  'load-movie-staff-info',
+  async (filmId: number | string, { extra: { client, requests } }: any) => {
+    return await client.get(requests.GET_MOVIE_STAFF_INFO_BY_ID(filmId), {
+      headers: {
+        'X-API-KEY': requests.MOVIES_API_KEY,
+      },
+    });
+  }
+);
+
+const initialState = {
+  movieData: {},
+  movieStaffData: {},
+};
+
+const movieInfo = createSlice({
+  name: 'movieInfo',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadMovieInfo.fulfilled, (state, action) => {
+        state.movieData = action.payload.data;
+      })
+      .addCase(loadMovieStaffInfo.fulfilled, (state, action) => {
+        state.movieStaffData = action.payload.data;
+      });
+  },
+});
+
+export const movieInfoReducer = movieInfo.reducer;
