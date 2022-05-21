8422cb196fa4eb2bb3aed36260dbb8401d9d202c
Oleg Somov <gog0125@mail.ru>
2022-05-21 20:43:08 +0300

    add firebase to dependencies

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,10 @@
 export { Header } from './Header/Header';
+export { LoadingSpinner } from './LoadingSpinner/LoadingSpinner';
+
 export { Dropdown } from './UI/Dropdown/Dropdown';
 export { Search } from './UI/Search/Search';
 export { Button } from './UI/Button/Button';
-export { LoadingSpinner } from './LoadingSpinner/LoadingSpinner';
+export { Ratings } from './UI/Ratings/Ratings';
 
 export { GameCard } from './GameCard/GameCard';
 export { MovieCard } from './MovieCard/MovieCard';
