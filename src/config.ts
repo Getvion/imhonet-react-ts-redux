dff383ef470a3a836cc01b3bf430d4a4430c1ed4
Oleg Somov <gog0125@mail.ru>
2022-05-24 15:05:45 +0300

    refactor: rename file and add types for arguments

--- src/config.ts
+++ src/config.ts
@@ -4,11 +4,11 @@ export const GAMES_API_KEY = 'key=2d5893a4192a410486b36abbd099f4cb';
 export const BASE_SHOWS = 'https://api.tvmaze.com';
 
 // games
-export const GET_BEST_GAMES = (pageNumber) => {
+export const GET_BEST_GAMES = (pageNumber: number | string) => {
   return `${GAMES_BASE}/games?${GAMES_API_KEY}&page=${pageNumber}`;
 };
 
-export const GET_GAME_INFO_BY_ID = (gameId) => {
+export const GET_GAME_INFO_BY_ID = (gameId: number | string) => {
   return `${GAMES_BASE}/games/${gameId}?${GAMES_API_KEY}`;
 };
 
@@ -17,18 +17,18 @@ export const GET_GAME_INFO_BY_ID = (gameId) => {
 export const MOVIES_BASE = 'https://kinopoiskapiunofficial.tech/api';
 export const MOVIES_API_KEY = '7dcd1d86-569b-4840-9c72-fa383b7b693a';
 
-export const GET_BEST_MOVIES = (pageNumber) => {
+export const GET_BEST_MOVIES = (pageNumber: number | string) => {
   return `${MOVIES_BASE}/v2.2/films/top?type=TOP_250_BEST_FILMS&page=${pageNumber}`;
 };
 
-export const GET_MOVIE_INFO_BY_ID = (filmId) => {
+export const GET_MOVIE_INFO_BY_ID = (filmId: number | string) => {
   return `${MOVIES_BASE}/v2.2/films/${filmId}`;
 };
 
-export const GET_MOVIE_STAFF_INFO_BY_ID = (filmId) => {
+export const GET_MOVIE_STAFF_INFO_BY_ID = (filmId: number | string) => {
   return `${MOVIES_BASE}/v1/staff?filmId=${filmId}`;
 };
 
-export const GET_MOVIE_SIMILAR_BY_ID = (filmId) => {
+export const GET_MOVIE_SIMILAR_BY_ID = (filmId: number | string) => {
   return `${MOVIES_BASE}/v2.2/films/${filmId}/similars`;
 };
