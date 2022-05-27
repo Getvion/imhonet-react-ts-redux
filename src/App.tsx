313ba2227fb4c5260499b93106b0f119e7001b6b
Oleg Somov <gog0125@mail.ru>
2022-05-27 19:43:44 +0300

    test: add test implementation with firestore database

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,62 @@ import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 
+//
+// import { db } from './firebase';
+// import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from 'firebase/firestore';
+//
+
 import { Header } from './components';
 import { Auth, Main, Profile, Games, Movies, Shows, Books, Search } from './pages';
 import { Book, Game, Movie, Show } from './pages/DetailsPages';
 
 function App() {
   const [shows, setShows] = useState([]);
-
   const BASE_SHOWS = 'https://api.tvmaze.com';
-
   useEffect(() => {
-    async function fetchData() {
+    const fetchData = async () => {
       await axios.get(`${BASE_SHOWS}/search/shows?q=girls`).then(({ data }) => setShows(data));
-    }
-
+    };
     fetchData();
-  }, []);
+  }, [shows]);
+
+  // const [users, setUsers] = useState({});
+  //
+  // const usersCollectionRef = collection(db, 'users');
+  // useEffect(() => {
+  // async function fetchData() {
+  //
+  // const data = await getDocs(usersCollectionRef);
+  //
+  // получаем данные из файрбейза и добавляем id юзера
+  // setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //
+  // const newUser = {
+  //   name: `${Math.random()}-name`,
+  //   surnname: `${Math.random()}-surname`,
+  //   age: Math.random() * 100,
+  // };
+  //
+  // добавить пользователя с данными newUser по пути usersCollectionRef
+  // await addDoc(usersCollectionRef, newUser);
+  //
+  // update data
+  // const updateUser = async (userId: string, prevAge: number) => {
+  // const userDoc = doc(db, 'users', userId);
+  //   const newFields = { age: prevAge + 1 };
+  //   await updateDoc(userDoc, newFields);
+  // };
+  // updateUser('zKHyk4XZQ39RTWteegns', 21);
+  //
+  // удалить пользователя
+  // const deleteUser = async (userId: string) => {
+  //   const userDoc = doc(db, 'users', userId);
+  //   deleteDoc(userDoc);
+  // };
+  // deleteUser('zKHyk4XZQ39RTWteegns');
+  // }
+  // fetchData();
+  // }, []);
 
   return (
     <div className='app'>
