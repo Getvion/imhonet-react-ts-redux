e5db3bb339a81ded52af1826952c8c31f4b49039
Oleg Somov <gog0125@mail.ru>
2022-05-28 11:33:25 +0300

    refactor: remove ratings component from UI folder to compoenents folder

---
+++ src/components/Ratings/Ratings.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+
+import exceptionalImg from './img/exceptional.png';
+import recomendedImg from './img/recomended.png';
+import mehImg from './img/meh.png';
+import skipImg from './img/skip.png';
+
+import classes from './Ratings.module.scss';
+
+export const Ratings = () => {
+  const [selectedRating, setSelectedRating] = useState(-1);
+
+  const ratingsData = [
+    { img: exceptionalImg, title: 'Великолепно', count: 0 },
+    { img: recomendedImg, title: 'Рекомендую', count: 0 },
+    { img: mehImg, title: 'Так себе', count: 0 },
+    { img: skipImg, title: 'Пропуск', count: 0 },
+  ];
+
+  const onSelectRating = (index: number) => {
+    setSelectedRating(index);
+  };
+
+  return (
+    <div className={classes.stat}>
+      {ratingsData.map((obj, index) => (
+        <div
+          key={obj.title}
+          className={clsx(classes.stat__item, { [classes.active]: selectedRating === index })}
+          onClick={() => onSelectRating(index)}
+        >
+          <img className={classes.stat__icon} src={obj.img} alt={obj.title} />
+          <div className={classes.buttons__label}>
+            <span className={classes.buttons__text}>{obj.title}</span>
+            <span className={classes.buttons__count}>{obj.count}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 export { Header } from './Header/Header';
 export { LoadingSpinner } from './LoadingSpinner/LoadingSpinner';
 export { SectionCard } from './SectionCard/SectionCard';
+export { Ratings } from './Ratings/Ratings';
 
 export { Dropdown } from './UI/Dropdown/Dropdown';
 export { Search } from './UI/Search/Search';
 export { Button } from './UI/Button/Button';
-export { Ratings } from './UI/Ratings/Ratings';
+export { AuthInput } from './UI/AuthInput/AuthInput';
