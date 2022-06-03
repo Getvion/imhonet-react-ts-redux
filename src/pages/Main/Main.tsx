4bb174eeaca5f0f23eb1ecc7f5d54bc0611fabc9
Oleg Somov <gog0125@mail.ru>
2022-06-03 23:11:04 +0300

    feat: remove static items and add slider for movies and games

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,161 +1,74 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { LoadingSpinner, SectionCard } from '../../components';
+import { loadBestGames } from '../../features/games/bestGamesSlice';
+import { loadBestMovies } from '../../features/movies/bestMoviesSlice';
+import { AppDispatch } from '../../store';
 
 import classes from './Main.module.scss';
 
+interface IBestGames {
+  bestGames: { gamesList: { results: { background_image: string; id: number; name: string }[] } };
+}
+
+interface IBestMovies {
+  bestMovies: {
+    moviesList: { films: { filmId: number; nameRu: string; nameEn: string; posterUrlPreview: string }[] };
+  };
+}
+
 export const Main = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const bestGames = useSelector((state: IBestGames) => state.bestGames.gamesList.results);
+  const bestMovies = useSelector((state: IBestMovies) => state.bestMovies.moviesList.films);
+
+  useEffect(() => {
+    dispatch(loadBestGames());
+    dispatch(loadBestMovies());
+  }, [dispatch]);
+
   return (
-    <div className={classes.container}>
+    <main className={classes.main}>
       <div className={classes.category}>
-        <h2 className={classes.category__title}>Новые Фильмы</h2>
+        <h2 className={classes.category__title}>Лучшие Фильмы</h2>
         <div className={classes.cards}>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
+          {bestMovies ? (
+            <Swiper slidesPerView={4} spaceBetween={30} loop={true}>
+              {bestMovies.map(({ filmId, nameEn, nameRu, posterUrlPreview }) => (
+                <SwiperSlide key={filmId}>
+                  <SectionCard
+                    name={nameRu || nameEn}
+                    bgImage={posterUrlPreview}
+                    section={'movies'}
+                    id={filmId}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          ) : (
+            <LoadingSpinner />
+          )}
         </div>
       </div>
-
       <div className={classes.category}>
-        <h2 className={classes.category__title}>Новые Сериалы</h2>
+        <h2 className={classes.category__title}>Лучшие Игры</h2>
         <div className={classes.cards}>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
+          {bestGames ? (
+            <Swiper slidesPerView={4} spaceBetween={30} loop={true}>
+              {bestGames.map(({ id, name, background_image }) => (
+                <SwiperSlide key={id}>
+                  <SectionCard name={name} bgImage={background_image} section={'games'} id={id} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          ) : (
+            <LoadingSpinner />
+          )}
         </div>
       </div>
-
-      <div className={classes.category}>
-        <h2 className={classes.category__title}>Новые Игры</h2>
-        <div className={classes.cards}>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-        </div>
-      </div>
-
-      <div className={classes.category}>
-        <h2 className={classes.category__title}>Новые Книги</h2>
-        <div className={classes.cards}>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-          <div className={classes.card}>
-            <img
-              className={classes.card__img}
-              src='https://static.wikia.nocookie.net/batman/images/9/9a/The_Batman_poster_10.jpg'
-              alt='movie name'
-            />
-            <h3 className={classes.card__name}>Movie Name</h3>
-          </div>
-        </div>
-      </div>
-    </div>
+    </main>
   );
 };
