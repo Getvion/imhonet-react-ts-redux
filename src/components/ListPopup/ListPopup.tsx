02114c8ad410f95b048d3a65e40fcfe099f20a71
Oleg Somov <gog0125@mail.ru>
2022-06-13 12:48:46 +0300

    feat: add title and description

--- src/components/ListPopup/ListPopup.tsx
+++ src/components/ListPopup/ListPopup.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { SectionCard } from '../SectionCard/SectionCard';
+import { Button } from '../UI/Button/Button';
 
 import classes from './ListPopup.module.scss';
 
@@ -8,6 +9,8 @@ interface PopupProps {
   showPopup: string;
   setShowPopup: Function;
   itemsArr: any;
+  title: string;
+  descr?: string;
 }
 
 interface IItem {
@@ -18,7 +21,9 @@ interface IItem {
   section: string;
 }
 
-export const ListPopup: React.FC<PopupProps> = ({ itemsArr, setShowPopup, showPopup }) => {
+export const ListPopup: React.FC<PopupProps> = ({ itemsArr, setShowPopup, showPopup, title, descr }) => {
+  // const [filteredArr, setFilteredArr] = useState(itemsArr);
+
   const onPopupClose = (e: any) => {
     const isOutsideClick = e.target.classList.contains(classes.modal);
     if (isOutsideClick) {
@@ -26,18 +31,34 @@ export const ListPopup: React.FC<PopupProps> = ({ itemsArr, setShowPopup, showPo
     }
   };
 
+  // const onDeleteItemFromList = (nameToDelete: string) => {
+  //   const filteredArr = itemsArr.filter((item: IItem) => item.name !== nameToDelete);
+
+  //   setFilteredArr(filteredArr);
+  //   // после удаления надо пушить все обратно в lists
+  // };
+
   return (
     <div className={classes.modal} onClick={onPopupClose}>
       <div className={classes.modal__dialog}>
+        <div className={classes.modal__text}>
+          <h3 className={classes.modal__title}>{title}</h3>
+          {descr ? <span className={classes.modal__descr}>{descr}</span> : null}
+        </div>
         <div className={classes.modal__content}>
           {itemsArr.map(({ id, bgImg, name, nameOrig, section }: IItem) => (
-            <SectionCard
-              key={id}
-              id={id}
-              bgImage={bgImg}
-              name={name || nameOrig}
-              section={section || showPopup}
-            />
+            <div className={classes.modal__element}>
+              <SectionCard
+                key={id}
+                id={id}
+                bgImage={bgImg}
+                name={name || nameOrig}
+                section={section || showPopup}
+              />
+              {/* <div className={classes.modal__delete}>
+                <Button onClick={() => onDeleteItemFromList(name)} text='Удалить' state='reject' />
+              </div> */}
+            </div>
           ))}
         </div>
       </div>
