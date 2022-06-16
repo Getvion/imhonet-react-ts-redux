a46395ae201dcba5cd4c58082df92e5d57eeab3a
Oleg Somov <gog0125@mail.ru>
2022-06-16 19:29:15 +0300

    feat: add adaptive for all displays in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,16 +8,19 @@ import { Dropdown } from '../UI/Dropdown/Dropdown';
 import { Search } from '../UI/Search/Search';
 
 import classes from './Header.module.scss';
+import { useWindowDimensions } from '../../hooks';
 
 export const Header = () => {
   const [currentPage, setCurrentPage] = useState(10);
   const [isMenuVisible, setIsMenuVisible] = useState(false);
 
+  const { windowWidth } = useWindowDimensions();
+
   const navArr = [
     { text: 'Игры', link: 'games' },
     { text: 'Фильмы', link: 'movies' },
-    { text: 'Сериалы', link: 'shows' },
-    { text: 'Книги', link: 'books' },
+    // { text: 'Сериалы', link: 'shows' },
+    // { text: 'Книги', link: 'books' },
   ];
 
   const onLinkClick = (index: number) => {
@@ -31,7 +34,7 @@ export const Header = () => {
         <Link to='/' className={classes.logo} onClick={() => setCurrentPage(-1)}>
           IMHONET
         </Link>
-        <Search />
+        {windowWidth > 600 ? <Search /> : null}
         <Dropdown />
         <div
           className={clsx(classes.burger__button, { [classes.visible]: isMenuVisible })}
@@ -41,13 +44,12 @@ export const Header = () => {
           <span className={classes.burger__line}></span>
           <span className={classes.burger__line}></span>
         </div>
-
         <div
           onClick={() => setIsMenuVisible(!isMenuVisible)}
           className={clsx(classes.nav__fade, { [classes.visible]: isMenuVisible })}
         ></div>
-
         <nav className={clsx(classes.nav, { [classes.visible]: isMenuVisible })}>
+          {windowWidth < 600 ? <Search /> : null}
           <ul className={classes.nav__list}>
             {navArr.map((obj, index) => (
               <li
