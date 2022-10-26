01a94ea34e2a01ac7dc9e76aae174423abec9549
Oleg Somov <gog0125@mail.ru>
2022-10-26 10:34:53 +0300

    add onBlur handler and correct types for props

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -5,17 +5,28 @@ import classes from './Input.module.scss';
 interface IProps {
   type?: string;
   placeholder: string;
-  setValue: Function;
+  setValue: (e: React.ChangeEvent<HTMLInputElement>) => void;
   value: string;
+  name?: string;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-export const Input: React.FC<IProps> = ({ type = 'text', placeholder, setValue, value }) => (
+export const Input: React.FC<IProps> = ({
+  type = 'text',
+  placeholder,
+  setValue,
+  value,
+  name,
+  onBlur
+}) => (
   <input
     value={value}
-    onChange={(e) => setValue(e.target.value)}
+    onChange={setValue}
     required
     type={type}
     className={classes.input}
     placeholder={placeholder}
+    name={name}
+    onBlur={onBlur}
   />
 );
